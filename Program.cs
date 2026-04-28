bd0bd2fcc219650060c96d6aa1966512894f0ac7
donjuan029 <juancarloandrade31@gmail.com>
2026-04-28 15:03:55 -0300

    :tada: init: Commit inicial

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿namespace Condicionais;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // int num = 10;
+
+        // if (num > 0)
+        // {
+        //     Console.WriteLine("Número positivo");
+        // }
+
+        // Console.WriteLine("Número negativo");
+
+        // Console.WriteLine("Digite um número: ");
+        // int num = int.Parse(Console.ReadLine());
+
+        // if(num > 0)
+        // {
+        //     Console.WriteLine("Positivo");
+        // }
+        // else if (num == 0)
+        // {
+        //     Console.WriteLine("O número é Zero");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Negativo");
+        // }
+
+        // Console.WriteLine("Digite sua idade: ");
+        // int idade = int.Parse(Console.ReadLine());
+
+        // if(idade < 16)
+        // {
+        //     Console.WriteLine("Não pode votar!!!");
+        // }
+        // else if (idade < 18)
+        // {
+        //     Console.WriteLine("Voto opcional");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Voto obrigatório");
+        // }
+
+        // Console.WriteLine("Digite a nota: ");
+        // double nota = double.Parse(Console.ReadLine());
+
+        // if(nota >= 7)
+        // {
+        //     Console.WriteLine("Aprovado");
+
+        //     if(nota >= 9)
+        //     {
+        //         Console.WriteLine("Aprovado e ganhou uma estrela!!!!"); //////////////////////////*****
+        //     }
+        // }
+        // else if(nota >= 5)
+        // {
+        //     Console.WriteLine("Recuperação");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Reprovado");
+        // }
+
+        // int n1 = 25;
+        // int n2 = 25;
+
+        // if(n1 > n2)
+        // {
+        //     Console.WriteLine("Pimeiro(n1) é maior");
+        // }
+        // else if(n2 > n1)
+        // {
+        //     Console.WriteLine("Segundo(n2) é maior");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Números iguais");
+        // }
+
+        // Console.WriteLine("Usuario: ");
+        // string usuario = Console.ReadLine();
+
+        // Console.WriteLine("Senha: ");
+        // string senha = Console.ReadLine();
+
+        // if(usuario == "admin" && senha == "1234")
+        // {
+        //     Console.WriteLine("Acesso permitido");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Acesso negado");
+        // }
+
+        // Console.WriteLine("Digite um número: ");
+        // int num = int.Parse(Console.ReadLine());
+
+        // if(num % 2 == 0)
+        // {
+        //     Console.WriteLine("Par");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Ímpar")  ;
+        // }
+
+        // int idade = 12;
+
+        // Console.WriteLine(idade >= 18 ? "Maior de idade" : "Menor de idade");
+
+        // // Switch case
+        // Console.WriteLine("Digite um número de 1 a 3: ");
+        // int opcao = int.Parse(Console.ReadLine());
+
+        // switch (opcao)
+        // {
+        //     case 1:
+        //         Console.WriteLine("Você escolheu a opção 1");
+        //         break;
+        //     case 2:
+        //         Console.WriteLine("Você escolheu a opção 2");
+        //         break;
+        //     case 3:
+        //         Console.WriteLine("Você escolheu a opção 3");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Opção inaválida");
+        //         break;
+        // }
+
+        // Console.WriteLine("Digite uma opção: (A, B ou C): ");
+        // string opcao = Console.ReadLine().ToUpper;
+        
+        // switch(opcao)
+        // {
+        //     case "A":
+        //         Console.WriteLine("Você escolheu a opção A");
+        //         break;
+        //     case "B":
+        //         Console.WriteLine("Você escolheu a opção B");
+        //         break;
+        //     case "C":
+        //         Console.WriteLine("Você escolheu a opção C");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Opção inaválida");
+        //         break;
+        // }
+
+        
+    }
+}
