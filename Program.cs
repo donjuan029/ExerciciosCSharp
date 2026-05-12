d38a509317ee0a89ce109eb3402330b77225c779
Juan Carlo <141068249+donjuan029@users.noreply.github.com>
2026-05-12 17:18:23 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,364 +1,48 @@
-﻿namespace Condicionais;
+using APIProdutos.Contexts;
+using APIProdutos.Interfaces;
+using APIProdutos.Repositories;
+using APIProdutos.Services;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 
-class Program
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+string? connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+if(string.IsNullOrWhiteSpace(connectionString))
 {
-    static void Main(string[] args)
-    {
-        // int num = 10;
-
-        // if (num > 0)
-        // {
-        //     Console.WriteLine("Número positivo");
-        // }
-
-        // Console.WriteLine("Número negativo");
-
-        // Console.WriteLine("Digite um número: ");
-        // int num = int.Parse(Console.ReadLine());
-
-        // if(num > 0)
-        // {
-        //     Console.WriteLine("Positivo");
-        // }
-        // else if (num == 0)
-        // {
-        //     Console.WriteLine("O número é Zero");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Negativo");
-        // }
-
-        // Console.WriteLine("Digite sua idade: ");
-        // int idade = int.Parse(Console.ReadLine());
-
-        // if(idade < 16)
-        // {
-        //     Console.WriteLine("Não pode votar!!!");
-        // }
-        // else if (idade < 18)
-        // {
-        //     Console.WriteLine("Voto opcional");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Voto obrigatório");
-        // }
-
-        // Console.WriteLine("Digite a nota: ");
-        // double nota = double.Parse(Console.ReadLine());
-
-        // if(nota >= 7)
-        // {
-        //     Console.WriteLine("Aprovado");
-
-        //     if(nota >= 9)
-        //     {
-        //         Console.WriteLine("Aprovado e ganhou uma estrela!!!!"); //////////////////////////*****
-        //     }
-        // }
-        // else if(nota >= 5)
-        // {
-        //     Console.WriteLine("Recuperação");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Reprovado");
-        // }
-
-        // int n1 = 25;
-        // int n2 = 25;
-
-        // if(n1 > n2)
-        // {
-        //     Console.WriteLine("Pimeiro(n1) é maior");
-        // }
-        // else if(n2 > n1)
-        // {
-        //     Console.WriteLine("Segundo(n2) é maior");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Números iguais");
-        // }
-
-        // Console.WriteLine("Usuario: ");
-        // string usuario = Console.ReadLine();
-
-        // Console.WriteLine("Senha: ");
-        // string senha = Console.ReadLine();
-
-        // if(usuario == "admin" && senha == "1234")
-        // {
-        //     Console.WriteLine("Acesso permitido");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Acesso negado");
-        // }
-
-        // Console.WriteLine("Digite um número: ");
-        // int num = int.Parse(Console.ReadLine());
-
-        // if(num % 2 == 0)
-        // {
-        //     Console.WriteLine("Par");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Ímpar")  ;
-        // }
-
-        // int idade = 12;
-
-        // Console.WriteLine(idade >= 18 ? "Maior de idade" : "Menor de idade");
-
-        // // Switch case
-        // Console.WriteLine("Digite um número de 1 a 3: ");
-        // int opcao = int.Parse(Console.ReadLine());
-
-        // switch (opcao)
-        // {
-        //     case 1:
-        //         Console.WriteLine("Você escolheu a opção 1");
-        //         break;
-        //     case 2:
-        //         Console.WriteLine("Você escolheu a opção 2");
-        //         break;
-        //     case 3:
-        //         Console.WriteLine("Você escolheu a opção 3");
-        //         break;
-        //     default:
-        //         Console.WriteLine("Opção inaválida");
-        //         break;
-        // }
-
-        // Console.WriteLine("Digite uma opção: (A, B ou C): ");
-        // string opcao = Console.ReadLine().ToUpper;
-        
-        // switch(opcao)
-        // {
-        //     case "A":
-        //         Console.WriteLine("Você escolheu a opção A");
-        //         break;
-        //     case "B":
-        //         Console.WriteLine("Você escolheu a opção B");
-        //         break;
-        //     case "C":
-        //         Console.WriteLine("Você escolheu a opção C");
-        //         break;
-        //     default:
-        //         Console.WriteLine("Opção inaválida");
-        //         break;
-        // }
-
-        
-        //Nível Fácil 1
-
-        // Console.WriteLine("Digite a temperatura: ");
-        // int temperatura = int.Parse(Console.ReadLine());
-
-        // if(temperatura >= 30)
-        // {
-        //     Console.WriteLine("Quente");
-        // }
-        // else if(temperatura >= 15)
-        // {
-        //     Console.WriteLine("Agradável");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Frio");
-        // }
-
-        //Nível Fácil 2
-
-        // Console.WriteLine("Digite um número: ");
-        // int num = int.Parse(Console.ReadLine());
-
-        // if(num > 100)
-        // {
-        //     Console.WriteLine("Número maior que 100");
-        // }
-        // else if(num == 100)
-        // {
-        //     Console.WriteLine("Número é 100");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Número é menor que 100");
-        // }
-
-        //Nível Fácil 3
-
-        // Console.WriteLine("Digite a senha: ");
-        // int senha = int.Parse(Console.ReadLine());
-
-        // if(senha == 123)
-        // {
-        //     Console.WriteLine("Senha válida");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Senha inváida");
-        // }
-
-        //Nível Fácil 4
-
-        // Console.WriteLine("Digite um número: ");
-        // int num = int.Parse(Console.ReadLine());
-
-        // if(num < 0)
-        // {
-        //     Console.WriteLine("Número negativo");
-        // }
-        // else if (num == 0)
-        // {
-        //     Console.WriteLine("Número neutro 0 ");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Número positivo");
-        // }
-
-        //Nível Fácil 5
-
-        // Console.WriteLine("Digite sua idade: ");
-        // int idade = int.Parse(Console.ReadLine());
-
-        // Console.WriteLine(idade >= 16 ? "Cadastro permitido" : "Cadastro não permitido");
-
-        //Nível Fácil 6
-
-        // Console.WriteLine("Digite um número (1, 2 ou 3)");
-        // int opcao = int.Parse(Console.ReadLine());
-
-        // switch(opcao)
-        // {
-        //     case 1:
-        //         Console.WriteLine("Você escolheu a opção 1 Manhã");
-        //         break;
-        //     case 2:
-        //         Console.WriteLine("Você escolheu a opção 2 Tarde");
-        //         break;
-        //     case 3:
-        //         Console.WriteLine("Você escolheu a opção 3 Noite");
-        //         break;
-        //     default:
-        //         Console.WriteLine("Opção inválida");
-        //         break;
-        // }
-
-        //Nível Fácil 7
-
-        // Console.WriteLine("Digite um número (1, 2 ou 3)");
-        // int opcao = int.Parse(Console.ReadLine());
-
-        // switch(opcao)
-        // {
-        //     case 1:
-        //         Console.WriteLine("Você escolheu a opção 1 Novo Jogo");
-        //         break;
-        //     case 2:
-        //         Console.WriteLine("Você escolheu a opção 2 Carregar Jogo");
-        //         break;
-        //     case 3:
-        //         Console.WriteLine("Você escolheu a opção 3 Sair");
-        //         break;
-        // }
-
-        //Nível Médio 1
-
-        // Console.WriteLine("Digite sua idade: ");
-        // int idade = int.Parse(Console.ReadLine());
-
-        // if(idade >= 18)
-        // {
-        //     Console.WriteLine("Adulto");
-        // }
-        // else if (idade >=13)
-        // {
-        //     Console.WriteLine("Adolescente");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Criança");
-        // }
-
-        //Nível Médio 2
-
-        // Console.WriteLine("Digite o primeiro número: ");
-        // int n1 = int.Parse(Console.ReadLine());
-
-        // Console.WriteLine("Digite o segundo número: ");
-        // int n2 = int.Parse(Console.ReadLine());
-
-        // Console.WriteLine(n1 > n2 ? "Primeiro número maior que o segundo" : "Segundo número maior que o primeiro");
-
-        //Nível Médio 3
-
-        // Console.WriteLine("Digite sua idade: ");
-        // int idade = int.Parse(Console.ReadLine());
-
-        // Console.WriteLine("Possui ingresso? Sim ou Não: ");
-        // string ingresso = Console.ReadLine().ToUpper();
-
-        // if(idade >= 18 || ingresso == "SIM")
-        // {
-        //     Console.WriteLine("Acesso permitido");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Acesso negado");
-        // }
-
-        //Nível Médio 4
-
-        // Console.WriteLine("Digite um número: ");
-        // int num = int.Parse(Console.ReadLine());
-
-        // if(num >= 0 && num <=10)
-        // {
-        //     Console.WriteLine("Valor válido");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Valor inválido");
-        // }
-
-        //Nível Médio 5
-
-        // Console.WriteLine("Digite sua idade: ");
-        // int idade = int.Parse(Console.ReadLine());
-
-        // if(idade < 18)
-        // {
-        //     Console.WriteLine("Que pena sem desconto");
-        // }
-        // else if(idade <60)
-        // {
-        //     Console.WriteLine("Parabéns ganhou 10% de desconto");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Parabéns ganhou 20% de desconto");
-        // }
-
-        //Nível Médio 6
-
-        // Console.WriteLine("Digite o valor da compra: ");
-        // float valorCompra = float.Parse(Console.ReadLine());
-
-        // Console.WriteLine(valorCompra >= 200 ? valorCompra*0.85 : valorCompra*0.95);
-
-        //Nível Médio 7
-
-        Console.WriteLine("Digite o primeiro número: ");
-        int n1 = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Digite o segundo número: ");
-        int n2 = int.Parse(Console.ReadLine());
-        
-        
-    }
+    throw new Exception("Connection n�o encontrada.");
 }
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Registra conex�o cm o banco (DBContext -> ProdutoContext)
+builder.Services.AddDbContext<ProdutoContext>(options => options.UseSqlServer(connectionString));
+
+// Inje��o de depend�ncias
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<ProdutoService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
